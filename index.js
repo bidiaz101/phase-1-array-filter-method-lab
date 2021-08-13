e1ba414d75261d322f9bb37ff43b7ee3555c3ea0
Brian Diaz <bidiaz101@gmail.com>
2021-08-13 17:01:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(names, string) {
+    return names.filter(function (names) {
+        return names.toLowerCase() === string.toLowerCase();
+    });
+};
+
+function fuzzyMatch (names, string) {
+    const x = string.length-1
+    return names.filter(function (names) {
+        return names.toLowerCase().charAt(x) === string.toLowerCase().charAt(x);
+    });
+};
+
+function matchName (driverObj, string) {
+    return driverObj.filter(function(obj){
+        return obj.name === string;
+    });
+};
