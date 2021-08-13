bf9613b2f453c28358b40e6dfe4f1cb020a9440c
Brian Diaz <bidiaz101@gmail.com>
2021-08-13 17:00:43 -0500

    commit tree cae13acbd3d71793d8ac763fe86e4313e520fefc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(names, string) {
+    return names.filter(function (names) {
+        return names.toLowerCase() === string.toLowerCase();
+    });
+};
+
+function fuzzyMatch (names, string) {
+    const x = string.length-1
+    return names.filter(function (names) {
+        return names.toLowerCase().charAt(x) === string.toLowerCase().charAt(x);
+    });
+};
+
+function matchName (driverObj, string) {
+    return driverObj.filter(function(obj){
+        return obj.name === string;
+    });
+};
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby', 'billy', 'billyrae', 'Billyrae'];
+
+console.log(fuzzyMatch(drivers, 'sA'));
